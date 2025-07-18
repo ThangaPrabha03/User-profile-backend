b6af251309be81f353d7fcc41e204e8bd59fdfac
ThangaPrabha03 <thangaprabha362005@gmail.com>
2025-07-18 12:22:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express(); 
+
+app.use(cors());        
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB Connected!!'))
+  .catch((err) => console.error('MongoDB Connection Error!!', err));
+
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
